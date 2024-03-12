9a4df4ad5484a6419594fb0b89bd7aaa3000450b
VÍCTOR SILVEIRA RODRIGUES <victor_rodrigues2@estudante.sesisenai.org.br>
2024-03-12 09:52:08 -0300

    alteraçoes

---
+++ src/componentes/Primeiro.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import  ReactDOM from "react-dom"
+
+
+import Bomdia from "./componentes/Primeiro"
+
+
+ReactDOM.render (<Bomdia name="Princesa"/>, document.getElementById('root'))
